788aef790b95151c46e14f967cda0c9699cd4351
pauguz1 <elsantana5@gmail.com>
2021-06-13 18:40:53 -0500

    se agrego el modelo de balance general y se añadio al admin

--- gestion/models.py
+++ gestion/models.py
@@ -1,3 +1,44 @@
 from django.db import models
-
+from django.conf import settings
+from datetime import date, datetime
 # Create your models here.
+
+class BalanceGeneral(models.Model):
+    #activo circulante
+    disponible = models.FloatField()
+    valoresNegociables = models.FloatField()
+    deudoresPorVenta = models.FloatField()
+    deudoresVarios = models.FloatField()
+    impuestosPorRecuperar = models.FloatField()
+
+
+    #pasivo circulante
+    cuentaPorPagar = models.FloatField()
+    proviciones = models.FloatField()
+    retenciones = models.FloatField()
+    convenioConInstituciones = models.FloatField()
+    otros = models.FloatField()
+
+    #activo fijo
+    mueblesUtiles = models.FloatField()
+    maquinariaEquipoOficina = models.FloatField()
+    otrosActivosFijos = models.FloatField()
+    equipoTransporte = models.FloatField()
+
+    #pasivo fijo
+    acredoresHipotecarios = models.FloatField()
+
+    #otros activos
+    programasComputacionales = models.FloatField()
+    otros2 = models.FloatField()
+    amortizaciones = models.FloatField()
+
+    #capital contable
+    capitalSocial = models.FloatField()
+    utilidadesRetenidas = models.FloatField()
+
+    fechaCreacion = models.DateField(default=datetime.now)
+    creador = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} {}'.format(self.creador,self.fechaCreacion)
